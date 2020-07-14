bb1f6b5d0b8ea80f624269964ae2bd9d8a9aac25
jdasilva80 <jdasilva1980@gmail.com>
2020-07-14 20:22:56 +0200

    comentar clase tests

--- src/test/java/com/jdasilva/socialweb/reclamaciones/app/SocialwebReclamacionesApplicationTests.java
+++ src/test/java/com/jdasilva/socialweb/reclamaciones/app/SocialwebReclamacionesApplicationTests.java
@@ -1,12 +1,12 @@
 package com.jdasilva.socialweb.reclamaciones.app;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SocialwebReclamacionesApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
